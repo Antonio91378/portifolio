8a4065e47b68fc0740ccfca6cee24303ada2e69f
Antonio Dias <antoniovictor200@gmail.com>
2024-01-14 14:58:29 -0300

    [Patch] Adicionando aba experiência

--- src/components/Data/ObjectivesData.tsx
+++ src/components/Data/ObjectivesData.tsx
@@ -1,34 +1,32 @@
 import React from 'react'
 
-const ObjectivesData: React.FC = () => {
-  return (
-    <p>
-      1. Continuar crescendo na área de{` `}
-      <span className="tech">Desenvolvimento de Software</span>, utilizando como
-      principais ferramentas o{` `}
-      <span className="lang">C#</span> e o{` `}
-      <span className="lang">Typescript</span>. O foco nessas linguagens é
-      vantajoso por serem amplamente utilizadas no mercado, tendo: Uma alta gama
-      de aplicabilidades; Frameworks escaláveis e bibliotecas favoráveis que
-      economizam tempo.
-      <br />
-      <br />
-      2. Ter como padrão de qualidade o desenvolvimento clean, utilizando uma
-      boa arquitetura, obedecendo boas práticas de programação que fazem com que
-      os princípios <span className="tech">SOLID</span> sejam pilares para um
-      bom código.
-      <br />
-      <br />
-      3. Desenvolver pensando sempre em deixar a aplicação segura, evitando
-      deixar brechas que comprometam a integridade das informações trafegadas.
-      <br />
-      <br />
-      4. Continuar sendo um grande entusiasta de tendências{' '}
-      <span className="lang">tech</span>, acompanhando por exemplo o
-      amadurecimento de novas aplicações <span className="tech"> VR e AR </span>
-      e aplicações de <span className="go">IOT</span>.
-    </p>
-  )
-}
+const ObjectivesData: React.FC = () => (
+  <p>
+    1. Continuar crescendo na área de{` `}
+    <span className="tech">Desenvolvimento de Software</span>, utilizando como
+    principais ferramentas o{` `}
+    <span className="lang">C#</span> e o{` `}
+    <span className="lang">Typescript</span>. O foco nessas linguagens é
+    vantajoso por serem amplamente utilizadas no mercado, tendo: Uma alta gama
+    de aplicabilidades; Frameworks escaláveis e bibliotecas favoráveis que
+    economizam tempo.
+    <br />
+    <br />
+    2. Ter como padrão de qualidade o desenvolvimento clean, utilizando uma boa
+    arquitetura, obedecendo boas práticas de programação que fazem com que os
+    princípios <span className="tech">SOLID</span> sejam pilares para um bom
+    código.
+    <br />
+    <br />
+    3. Desenvolver pensando sempre em deixar a aplicação segura, evitando deixar
+    brechas que comprometam a integridade das informações trafegadas.
+    <br />
+    <br />
+    4. Continuar sendo um grande entusiasta de tendências{` `}
+    <span className="lang">tech</span>, acompanhando por exemplo o
+    amadurecimento de novas aplicações <span className="tech"> VR e AR </span>e
+    aplicações de <span className="go">IOT</span>.
+  </p>
+)
 
 export default ObjectivesData
